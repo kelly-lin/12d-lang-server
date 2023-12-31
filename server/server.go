2ec0421fc2a9d20cbb186f82f22a0f11243cb419
Kelly Lin <findkellylin@gmail.com>
2023-12-31 21:40:34 +1100

    fix: check for no def err

--- server/server.go
+++ server/server.go
@@ -279,7 +279,6 @@ func (s *Server) handleMessage(msg protocol.RequestMessage) (protocol.ResponseMe
 			return protocol.ResponseMessage{}, 0, errors.New("source code not found")
 		}
 		identifierNode, err := parser.FindIdentifierNode(rootNode, params.Position.Line, params.Position.Character)
-		identifier := identifierNode.Content([]byte(sourceCode))
 		if errors.Is(err, parser.ErrNoDefinition) {
 			return protocol.ResponseMessage{
 					ID:     msg.ID,
@@ -288,6 +287,7 @@ func (s *Server) handleMessage(msg protocol.RequestMessage) (protocol.ResponseMe
 				len(protocol.NullResult),
 				nil
 		}
+		identifier := identifierNode.Content([]byte(sourceCode))
 		if err != nil {
 			return protocol.ResponseMessage{}, 0, err
 		}
