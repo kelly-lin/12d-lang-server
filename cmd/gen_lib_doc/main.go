a850aecf3c80b276482275135e5431c171b8d517
Kelly Lin <findkellylin@gmail.com>
2024-01-07 22:31:31 +1100

    chore: extract create markdown doc func

--- cmd/gen_lib_doc/main.go
+++ cmd/gen_lib_doc/main.go
@@ -6,6 +6,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/kelly-lin/12d-lang-server/protocol"
 	"io"
 	"os"
 	"regexp"
@@ -47,7 +48,7 @@ func main() {
 			matches := re.FindStringSubmatch(name)
 			if len(matches) > 1 {
 				funcName := matches[1]
-				agg[funcName] = append(agg[funcName], createDocMarkdownString(name, api.Desc))
+				agg[funcName] = append(agg[funcName], protocol.CreateDocMarkdownString(name, api.Desc))
 			}
 		}
 	}
@@ -79,22 +80,4 @@ type ManualItem struct {
 	Names []string `json:"names"`
 	Desc  string   `json:"description"`
 	ID    string   `json:"id"`
-}
-
-// Creates the documentation markdown string from the provided signature as
-// 12dpl code and description as markdown.
-func createDocMarkdownString(signature, desc string) string {
-	if signature == "" {
-		return desc
-	}
-	if desc == "" {
-		return fmt.Sprintf("```12dpl\\n%s\\n```", signature)
-	}
-	// If we use description directly we will be using the unescaped version
-	// we read in from the JSON file. This is problematic because we are
-	// generating code which requires the escaped version and causes issues when
-	// we are creating generated code with unescaped characters, such as quotes.
-	// We are marshalling description again to get the escaped version.
-	encodedDesc, _ := json.Marshal(desc)
-	return fmt.Sprintf("```12dpl\\n%s\\n```\\n---\\n%s", signature, encodedDesc[1:len(encodedDesc)-1])
 }
---
+++ protocol/doc.go
@@ -0,0 +1,24 @@
+package protocol
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Creates the documentation markdown string from the provided signature as
+// 12dpl code and description as markdown.
+func CreateDocMarkdownString(signature, desc string) string {
+	if signature == "" {
+		return desc
+	}
+	if desc == "" {
+		return fmt.Sprintf("```12dpl\\n%s\\n```", signature)
+	}
+	// If we use description directly we will be using the unescaped version
+	// we read in from the JSON file. This is problematic because we are
+	// generating code which requires the escaped version and causes issues when
+	// we are creating generated code with unescaped characters, such as quotes.
+	// We are marshalling description again to get the escaped version.
+	encodedDesc, _ := json.Marshal(desc)
+	return fmt.Sprintf("```12dpl\\n%s\\n```\\n---\\n%s", signature, encodedDesc[1:len(encodedDesc)-1])
+}
